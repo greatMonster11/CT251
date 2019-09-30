86dce1ce9d3de0026e8ebd011c7f8a7f4f982636
Phuoc Thanh <39474278+greatMonster11@users.noreply.github.com>
2019-09-30 10:38:28 +0700

    Add files via upload

---
+++ Excercise3/Hello/Hello/DangNhap.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hello
+{
+    public partial class DangNhap : Form
+    {
+        public DangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string thongbao = "";
+            thongbao += "Ten dang nhap la: ";
+            thongbao += this.textUser.Text;
+            thongbao += "\n\rMat khau la: ";
+            thongbao += this.textPass.Text;
+            if (this.chkRemem.Checked == true)
+                thongbao += "\n\rBan co ghi nho tai khoan";
+            MessageBox.Show(thongbao, "Tiêu đề", MessageBoxButtons.OKCancel, MessageBoxIcon.Information); 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.textPass.ResetText();
+            this.textUser.ResetText();
+            this.textUser.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Using dialog for ending process
+            DialogResult answer;
+            answer = MessageBox.Show("Are you sure ?? ", "Answer",
+            MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if(answer == DialogResult.OK) 
+                Application.Exit();
+        }
+    }
+}
---
+++ Excercise3/Hello/MathForDumies/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace MathForDumies
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textNo1 = new System.Windows.Forms.TextBox();
+            this.textNo2 = new System.Windows.Forms.TextBox();
+            this.textResult = new System.Windows.Forms.TextBox();
+            this.btnCong = new System.Windows.Forms.Button();
+            this.btnTru = new System.Windows.Forms.Button();
+            this.btnNhan = new System.Windows.Forms.Button();
+            this.bntChia = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(82, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập số thứ 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(82, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kết quả là: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(82, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nhập số thứ 2:";
+            // 
+            // textNo1
+            // 
+            this.textNo1.Location = new System.Drawing.Point(264, 71);
+            this.textNo1.Name = "textNo1";
+            this.textNo1.Size = new System.Drawing.Size(100, 20);
+            this.textNo1.TabIndex = 3;
+            // 
+            // textNo2
+            // 
+            this.textNo2.Location = new System.Drawing.Point(264, 114);
+            this.textNo2.Name = "textNo2";
+            this.textNo2.Size = new System.Drawing.Size(100, 20);
+            this.textNo2.TabIndex = 4;
+            // 
+            // textResult
+            // 
+            this.textResult.Location = new System.Drawing.Point(264, 163);
+            this.textResult.Name = "textResult";
+            this.textResult.ReadOnly = true;
+            this.textResult.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textResult.Size = new System.Drawing.Size(100, 20);
+            this.textResult.TabIndex = 5;
+            // 
+            // btnCong
+            // 
+            this.btnCong.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCong.Location = new System.Drawing.Point(80, 211);
+            this.btnCong.Name = "btnCong";
+            this.btnCong.Size = new System.Drawing.Size(59, 36);
+            this.btnCong.TabIndex = 6;
+            this.btnCong.Text = "+";
+            this.btnCong.UseVisualStyleBackColor = true;
+            this.btnCong.Click += new System.EventHandler(this.btnCong_Click);
+            // 
+            // btnTru
+            // 
+            this.btnTru.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTru.Location = new System.Drawing.Point(145, 211);
+            this.btnTru.Name = "btnTru";
+            this.btnTru.Size = new System.Drawing.Size(59, 36);
+            this.btnTru.TabIndex = 7;
+            this.btnTru.Text = "-";
+            this.btnTru.UseVisualStyleBackColor = true;
+            this.btnTru.Click += new System.EventHandler(this.btnTru_Click);
+            // 
+            // btnNhan
+            // 
+            this.btnNhan.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNhan.Location = new System.Drawing.Point(210, 211);
+            this.btnNhan.Name = "btnNhan";
+            this.btnNhan.Size = new System.Drawing.Size(59, 36);
+            this.btnNhan.TabIndex = 8;
+            this.btnNhan.Text = "*";
+            this.btnNhan.UseVisualStyleBackColor = true;
+            this.btnNhan.Click += new System.EventHandler(this.btnNhan_Click);
+            // 
+            // bntChia
+            // 
+            this.bntChia.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntChia.Location = new System.Drawing.Point(278, 211);
+            this.bntChia.Name = "bntChia";
+            this.bntChia.Size = new System.Drawing.Size(59, 36);
+            this.bntChia.TabIndex = 9;
+            this.bntChia.Text = "/";
+            this.bntChia.UseVisualStyleBackColor = true;
+            this.bntChia.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.Location = new System.Drawing.Point(343, 211);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(59, 36);
+            this.btnXoa.TabIndex = 10;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 288);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.bntChia);
+            this.Controls.Add(this.btnNhan);
+            this.Controls.Add(this.btnTru);
+            this.Controls.Add(this.btnCong);
+            this.Controls.Add(this.textResult);
+            this.Controls.Add(this.textNo2);
+            this.Controls.Add(this.textNo1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Math for dumies";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textNo1;
+        private System.Windows.Forms.TextBox textNo2;
+        private System.Windows.Forms.TextBox textResult;
+        private System.Windows.Forms.Button btnCong;
+        private System.Windows.Forms.Button btnTru;
+        private System.Windows.Forms.Button btnNhan;
+        private System.Windows.Forms.Button bntChia;
+        private System.Windows.Forms.Button btnXoa;
+    }
+}
+
